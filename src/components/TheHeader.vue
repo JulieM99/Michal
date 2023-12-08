d6df5d42ed66b7ab57a7aa2dc502fa7d606c9d62
JulieM99 <julia.mosinska990426@gmail.com>
2023-12-08 21:39:17 +0100

    'pieknanazwa'

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -10,24 +10,38 @@
         </div>
 
         <div class="lang-style">
-            <label for="language">Language</label>
-            <div class="language-dropdown">
-                <select name="language" id="language" class="language-select">
-                    <option value="polish" data-flag="🇵🇱">Polish</option>
-                    <option value="english" data-flag="🇬🇧">English</option>
-                    <option value="ukrainian" data-flag="🇺🇦">Ukrainian</option>
-                </select>
-                <span class="selected-flag">🇵🇱</span>
+            <div class="selected-lang">
+                  English
             </div>
+
+              <ul>
+                  <li>
+                        <a href="#" class="de">German</a>
+                  </li>
+                  <li>
+                        <a href="" class="en">English</a>
+                  </li>
+                  <li>
+                        <a href="" class="fr">French</a>
+                  </li>
+                  <li>
+                        <a href="" class="pl">Polish</a>
+                  </li>
+                  <li>
+                        <a href="" class="ua">Ukrainian</a>
+                  </li>
+              </ul>
+
         </div>
 
     </header>
-
     
 </template>
 
 
 <script>
+   
+   
 </script>
 
 
@@ -51,42 +65,112 @@ header {
     color: white;
     box-shadow: 0 0 15px #000000;
 }
+
+/* FLAGI W MENU */
 .lang-style {
     display: flex;
     flex-direction: column;
-    gap: 5px;
+    gap: 10px;
+    line-height: 2;
+    width: 100px;
 }
 
-.language-dropdown {
-  position: relative;
-  display: inline-block;
-}
-
-.language-select {
-  appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  padding: 5px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  background-color: white;
+.lang-style .selected-lang {
+  display: flex;
+  justify-content: space-between;
+  line-height: 2;
   cursor: pointer;
 }
 
-.selected-flag {
-  position: absolute;
-  right: 10px;
-  top: 50%;
-  transform: translateY(-50%);
+.lang-style .selected-lang:before {
+    content: '';
+    display: inline-block;
+    width: 32px;
+    height: 32px;
+    background-image: url(https://flagsapi.com/GB/flat/32.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+.selected-lang:before {
+    content: '';
+    display: block;
+    width: 32px;
+    height: 32px;
+    background-image: url(https://flagsapi.com/GB/flat/32.png);
 }
 
+.lang-style ul {
+  margin: 0;
+    padding: 0;
+    display: none;
+    background-color: #fff;
+    border: 1px solid #f8f8f8;
+    position: absolute;
+    top: 45px;
+    right: 0px;
+    width: 125px;
+    border-radius: 5px;
+    box-shadow: 0px 1px 10px rgba(0,0,0,0.2);
+}
+
+.lang-style ul li {
+  list-style: none;
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+}
+
+.lang-style ul li a{
+  text-decoration: none;
+  width: 125px;
+  padding: 5px 10px;
+  display: block;
+}
+
+.lang-style ul li:hover {
+  background-color: #f2f2f2;
+}
+
+.lang-style ul li a:before {
+  content: '';
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  vertical-align: middle;
+  margin-right: 10px;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.de:before {
+    background-image: url(https://flagsapi.com/DE/flat/32.png);
+}
+
+.en:before {
+    background-image: url(https://flagsapi.com/GB/flat/32.png);
+}
+.fr:before {
+    background-image: url(https://flagsapi.com/FR/flat/32.png);
+}
+
+.pl:before {
+    background-image: url(https://flagsapi.com/PL/flat/32.png);
+}
+
+.ua:before {
+    background-image: url(https://flagsapi.com/UA/flat/32.png);
+}
+
+.lang-style:hover ul {
+    display: block;
+}
+
+/* PODKREŚLENIE MENU */
 .menu-style {
     display: flex;
     align-items: center;
     gap: 20px;
 }
-
-
 
 .menu-style a {
   margin: 10px;
---
+++ src/views/App.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <TheHeader/>
+  
+  <div class="wrapper">
+    <router-view/>
+  </div>
+
+  <TheFooter/>
+</template>
+
+<script>
+  import TheFooter from './components/TheFooter.vue'
+  import TheHeader from './components/TheHeader.vue'
+
+
+  export default {
+    components: {
+      TheFooter,
+      TheHeader
+    }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&family=IBM+Plex+Sans:wght@200&family=Oswald:wght@300&display=swap');
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold; 
+  color: white;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+* {
+  margin: 0px;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: rgba(235, 220, 196, 0.801); 
+  /* background-image: url("./assets/sky-blue-flag-poland.jpg");
+  background-repeat: no-repeat;
+  background-size: cover; */
+}
+
+.wrapper {
+  min-height: 100vh;
+}
+
+
+
+</style>
+
